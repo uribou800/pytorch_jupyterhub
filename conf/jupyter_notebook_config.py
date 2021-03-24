65460ff09aabb22ddfe141aa2b8dbf65df788e9d
uribou800 <uribou1800@gmail.com>
2021-03-25 02:13:26 +0900

    PR用

--- conf/jupyter_notebook_config.py
+++
@@ -1,4 +0,0 @@
-c = get_config()
-c.NotebookApp.ip = '0.0.0.0'
-c.NotebookApp.open_browser = False
-c.NotebookApp.port = 8080
--- src/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-echo "change user_id"
-
-usermod -u $USER_ID -o -m developer
-groupmod -g $GROUP_ID developer
